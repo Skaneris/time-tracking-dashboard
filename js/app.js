27e8e776ac32dc858ccfe16f985a253d7c34a90b
Sergey Prijan <dneprserp355@gmail.com>
2021-09-28 21:02:21 +0300

    complete rendetHTML func in js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 let data
 
+renderCards();
 getData();
+
+function renderCards() {
+    const cards = ['Work', 'Play', 'Study', 'Exercise', 'Social', 'Self Care'];
+    const wrapper = document.querySelector('.cards')
+    cards.map(item => {
+        wrapper.insertAdjacentHTML('beforeend', `
+        <div class="card ${ item.toLowerCase() }">
+            <div class="card__body">
+                <div class="card__top">
+                    <h4>${item}</h4>
+                    <button>
+                        <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg>
+                    </button>
+                </div>
+                <div class="card__bottom">
+                    <div class="left" data-type="${item}"></div>
+                    <div class="right" data-type="${item}-prev"></div>
+                </div>
+            </div>
+        </div>
+        `);
+    })
+}
 
 async function getData() {
     data = await fetchData('/data.json');
